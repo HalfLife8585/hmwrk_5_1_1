09ac36b2f9c0519222bef548154f55d3bb34fcf7
HalfLife85 <aleksandrmusic@gmail.com>
2024-07-15 11:56:35 +0300

    My_commit5

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ user_input = input("Введіть ім'я змінної: ")
 
 valid = (
     user_input and
-    not user_input[0].isdigit() and
-    user_input.islower() and
-    user_input.count('_') <= 1 and
-    all(char.isalnum() or char == '_' for char in user_input) and
-    user_input not in keyword.kwlist
+    (user_input == '_' or
+     (not user_input[0].isdigit() and
+      user_input.islower() and
+      user_input.count('_') <= 1 and
+      all(char.isalnum() or char == '_' for char in user_input) and
+      user_input not in keyword.kwlist))
 )
 
 print(valid)

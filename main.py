7098e44791897914983fd9225f09f194a3c92fbf
HalfLife85 <aleksandrmusic@gmail.com>
2024-07-15 11:58:37 +0300

    My_commit5

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ valid = (
     (user_input == '_' or
      (not user_input[0].isdigit() and
       user_input.islower() and
-      user_input.count('_') <= 1 and
       all(char.isalnum() or char == '_' for char in user_input) and
       user_input not in keyword.kwlist))
 )
